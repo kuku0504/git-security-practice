b70a5fa41feba5755d9cd6acb8d2d15cc380b5f2
TL <TeamLead19@gmail.com>
2025-07-22 16:08:27 +0530

    added method with spring security dependency

--- Spring_Security_Proj1/src/main/java/com/nt/controller/SecurityOperationController.java
+++ Spring_Security_Proj1/src/main/java/com/nt/controller/SecurityOperationController.java
@@ -1,11 +1,13 @@
 package com.nt.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/security-api")
 public class SecurityOperationController {
+	@GetMapping("/msg")
       public String getMsg() {
     	  return "good morning kuku";
       }
