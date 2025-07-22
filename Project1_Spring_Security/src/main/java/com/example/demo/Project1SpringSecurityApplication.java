40d29802d3ea21e992c0499610c6aeed4e6dd286
TL <TeamLead19@gmail.com>
2025-07-22 21:30:13 +0530

    fresh method created

---
+++ Project1_Spring_Security/src/main/java/com/example/demo/Project1SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1SpringSecurityApplication.class, args);
+	}
+
+}
---
+++ Project1_Spring_Security/src/main/java/com/nt/controller/DemoOperationController.java
@@ -0,0 +1,15 @@
+package com.nt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/security-api")
+public class DemoOperationController {
+    @GetMapping("/msg")  
+	public String home() {
+		return "good morning";
+	}
+    
+}
